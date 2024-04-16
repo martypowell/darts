a9db43be71d60fc3cffa74bbb67988740c821a76
Marty Powell <jpowel25@gmail.com>
2024-04-16 17:44:56 -0400

    removed unused props

--- static/icons/DoubleScoreIcon.tsx
+++ static/icons/DoubleScoreIcon.tsx
@@ -1,10 +1,9 @@
-export const DoubleScoreIcon = (props) => (
+export const DoubleScoreIcon = () => (
   <svg
     width="100"
     height="100"
     viewBox="0 0 100 100"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
   >
     <line x1="10" y1="10" x2="90" y2="90" stroke="black" stroke-width="5" />
     <line x1="10" y1="90" x2="90" y2="10" stroke="black" stroke-width="5" />
